02116df0bded8f8a7fd32b6fbf4c45d0731472e3
grietje ogink <grietje.ogink@kiesraad.nl>
2025-11-21 11:38:51 +0100

    use describe block

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,9 @@
 import { test, expect } from '@playwright/test';
 
+
+test.describe('User can buy products from the shop', async() => {
+
+
 test('User can buy a Sauce Labs Backpack', async ({ page }) => {
 
   await page.goto('https://www.saucedemo.com/');
@@ -13,8 +17,7 @@ test('User can buy a Sauce Labs Backpack', async ({ page }) => {
   await page.getByText('Sauce Labs Backpack').click();
   await expect(page.getByTestId('back-to-products')).toBeVisible();
 
-  await page.getByTestId('back-to-products').click();
-  await page.getByTestId('add-to-cart-sauce-labs-backpack').click();
+  await page.getByText('Add to cart').click();
   await page.getByTestId('shopping-cart-link').click();
   await expect(page.getByTestId('checkout')).toBeVisible();
 
@@ -27,4 +30,30 @@ test('User can buy a Sauce Labs Backpack', async ({ page }) => {
   await expect(page.getByText('Thank you for your order!')).toBeVisible();
 });
 
+test('User can buy a Sauce Labs Bike light', async ({ page }) => {
 
+  await page.goto('https://www.saucedemo.com/');
+
+  await page.getByPlaceholder('Username').fill('standard_user');
+  await page.getByPlaceholder('Password').fill('secret_sauce');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await expect(page.getByTestId('shopping-cart-link')).toBeVisible();
+
+  await page.getByText('Sauce Labs Bike Light').click();
+  await expect(page.getByTestId('back-to-products')).toBeVisible();
+
+  await page.getByText('Add to cart').click();
+  await page.getByTestId('shopping-cart-link').click();
+  await expect(page.getByTestId('checkout')).toBeVisible();
+
+  await page.getByTestId('checkout').click();
+  await page.getByPlaceholder('First Name').fill('Grietje');
+  await page.getByPlaceholder('Last Name').fill('Ogink');
+  await page.getByPlaceholder('Zip/Postal Code').fill('1234');
+  await page.getByRole('button', { name: 'Continue' }).click();
+  await page.getByTestId('finish').click();
+  await expect(page.getByText('Thank you for your order!')).toBeVisible();
+});
+
+})
